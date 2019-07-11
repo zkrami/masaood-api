e494de13c4b849d4cb3b243ec1ec46a66e94a23a
Rami Al-Zebak <17zrami@gmail.com>
2019-07-11 16:50:40 +0300

    user model

---
+++ user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    mobile = models.CharField(max_length=255, unique=True)
+
+    pass
