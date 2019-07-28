ac45d102b3e33d0e2314b9e128fd9e7fca594074
Rami Al-Zebak <17zrami@gmail.com>
2019-07-28 17:19:29 +0300

    user,center status

--- center/models.py
+++ center/models.py
@@ -3,12 +3,24 @@ from django.conf import settings
 from state.models import State
 import media.models
 
+import enum
+# Create your models here.
+
+
+class StatusEnum(enum.Enum):
+    activated = "activated"
+    deactivated = "deactivated"
+
+    def toChoices():
+        return ((StatusEnum.activated.value, "activated"),
+                (StatusEnum.deactivated.value, "deactivated"))
 
 class Center(models.Model):
     nameEn = models.CharField(max_length=255)
     nameAr = models.CharField(max_length=255)
     lat = models.FloatField()
     lng = models.FloatField()
+    status = models.CharField(max_length=100 , default=StatusEnum.activated.value , choices = StatusEnum.toChoices())
     createdAt = models.DateTimeField(auto_now=True)
     states = models.ManyToManyField(State, related_name="centers")
 
--- user/models.py
+++ user/models.py
@@ -2,8 +2,18 @@ from django.db import models
 from django.contrib.admin import ModelAdmin
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
+import enum
 # Create your models here.
 import rest_framework
+
+
+class StatusEnum(enum.Enum):
+    activated = "activated"
+    deactivated = "deactivated"
+
+    def toChoices():
+        return ((StatusEnum.activated.value, "activated"),
+                (StatusEnum.deactivated.value, "deactivated"))
 
 class User(AbstractUser):
     mobile = models.CharField(max_length=255, unique=True, blank=True)
@@ -15,7 +25,7 @@ class User(AbstractUser):
     )
     verified = models.BooleanField(default=False)
     email = models.EmailField(null=True, unique=True , default=None)
-
+    status = models.CharField(max_length=100 , default=StatusEnum.activated.value , choices = StatusEnum.toChoices())
     search_fields = ('id', )
     autocomplete_fields = ('id', )
 
