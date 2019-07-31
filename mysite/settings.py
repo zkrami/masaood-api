fb3bb679d15fbc4b384cbf2363dfecffc64647d9
Rami Al-Zebak <17zrami@gmail.com>
2019-07-31 15:52:57 +0300

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'rf4@13_be6red4^+uw=k*yash4f^q9j4*@)8#10^h6pbj78r_8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['104.217.253.15', '174.138.28.26',
+ALLOWED_HOSTS = ['174.138.28.26', '104.217.253.15', '174.138.28.26',
                  'localhost', 'localhost:4200']
 
 
