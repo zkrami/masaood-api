0095ac9cc657eeddfadfcd62ab5c6ceba4ea21bf
Rami Al-Zebak <17zrami@gmail.com>
2019-08-05 14:58:07 +0300

    default status

--- product/models.py
+++ product/models.py
@@ -55,8 +55,8 @@ class Product(models.Model):
                      ("available", "available"))
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     code = models.CharField(max_length=255)
-    barcode = models.CharField(max_length=255, null=True)   
-    status = models.CharField(max_length=20, choices=StatusChoices)
+    barcode = models.CharField(max_length=255, null=True)
+    status = models.CharField(max_length=20, choices=StatusChoices, default="available")
     price = models.DecimalField(default=0,  decimal_places=4, max_digits=10)
 
     abstractProduct = models.ForeignKey(
