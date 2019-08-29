63f6aaeb23a6609177cbaf6f0d5696b03e7d747c
Rami Al-Zebak <17zrami@gmail.com>
2019-08-29 16:37:30 +0300

    etisalat api

--- mobileauth/views.py
+++ mobileauth/views.py
@@ -2,12 +2,13 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from django.utils.crypto import get_random_string
+from django.utils import timezone
 from user.models import User
 from rest_framework.authtoken.models import Token
 from user.serializers import UserDetailsSerializer
 from django.contrib.auth.models import Group
 import requests
-from datetime import datetime
+from datetime import datetime , timedelta
 import json
 import base64
 from .errors import ServiceUnavailable, MobileVerificationError
@@ -17,59 +18,18 @@ from .models import VerificationToken
 # @todo refactor
 
 
-def verifiy(mobile, code):
-
-    payload = {
-        'method': 'sms',
-        'sms': {
-            'code': code
-        }
-    }
-
-    key = "dcf8de29-f0fb-4e2b-9b7c-fa2e1d8766bc"
-
-    secret = "l1tQtF/p90+O2xBZjhiB+Q=="
-    b64bytes = base64.b64encode(
-        ('application:%s:%s' % (key, secret)).encode())
-    auth = 'basic %s' % b64bytes.decode('ascii')
-
-    headers = {
-        'cache-control': 'no-cache',
-        'Content-Type': 'application/json',
-        'x-timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S %p"),
-        'Authorization': auth
-    }
-
-    result = requests.put("https://verificationapi-v1.sinch.com/verification/v1/verifications/number/" + mobile,
-                          json=payload, headers=headers)
-
-    return result
+def send_verification(mobile , code):
 
 
-def send_verification(mobile):
+    username = "digital1"
+    password = "eidadha2019"
 
-    payload = {
-        'method': 'sms',
-        'identity': {
-            'type': 'number',
-            'endpoint': mobile
-        }
-    }
-    key = "dcf8de29-f0fb-4e2b-9b7c-fa2e1d8766bc"
-    secret = "l1tQtF/p90+O2xBZjhiB+Q=="
-    b64bytes = base64.b64encode(
-        ('application:%s:%s' % (key, secret)).encode())
-    auth = 'basic %s' % b64bytes.decode('ascii')
-    headers = {
-        'cache-control': 'no-cache',
-        'Content-Type': 'application/json',
-        'x-timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S %p"),
-        'Authorization': auth
-    }
+    timedef = (datetime.utcnow() + timedelta(seconds=0)).strftime("%Y%m%d%H%M%S")
 
-    result = requests.post("https://verificationapi-v1.sinch.com/verification/v1/verifications",
-                           json=payload, headers=headers)
+    message = "{0} is your verification code for Al Masaood Tailoring account".format(code) 
 
+    result = requests.get("http://messaging.etisalat.ae/bms/soap/Messenger.asmx/HTTP_SendSms?customerID=13528&userName={0}&userPassword={1}&originator=Test 1&smsText={2}&recipientPhone={3}&messageType=Latin&defDate={4}&blink=false&flash=false&Private=false".format(username , password ,  message , mobile , timedef))
+    print("http://messaging.etisalat.ae/bms/soap/Messenger.asmx/HTTP_SendSms?customerID=13528&userName={0}&userPassword={1}&originator=Test 1&smsText={2}&recipientPhone={3}&messageType=Latin&defDate={4}&blink=false&flash=false&Private=false".format(username , password ,  message , mobile , timedef))
     return result
 
 
@@ -80,40 +40,32 @@ class MobileAuthViewSet(viewsets.ViewSet):
     def login(self, request):
 
         mobile = request.data["mobile"]
+        code = get_random_string(length=6, allowed_chars='1234567890')
+        verification_result = send_verification(mobile, code)
 
-        verification_result = send_verification(mobile)
-
-        #if verification_result.status_code != 200:
-         #   raise ServiceUnavailable()
+        if verification_result.status_code != 200:
+            raise ServiceUnavailable()
 
         user, created = User.objects.get_or_create(mobile=mobile)
-        # add user role
         group = Group.objects.get(name='user')
         user.groups.add(group)
 
-        code = get_random_string(length=6, allowed_chars='1234567890')
         token, tokenCreated = VerificationToken.objects.get_or_create(key=mobile, code=code)
 
-        return Response({"message": "Created successfully", "verified": user.verified , "code" : code })
+        return Response({"message": "Created successfully", "verified": user.verified, "code": code})
 
     @action(detail=False, methods=['post'])
     def verify(self, request):
         mobile = request.data["mobile"]
         code = request.data["code"]
-        if code != "666666":                    
-            # @temporairly 
-            try:                
-                VerificationToken.objects.get(key=mobile, code=code)                
+        if code != "666666":
+            try:
+                
+                
+                threshold = timezone.now() - timedelta(minutes=60) # code is valid for 60 minutes 
+                VerificationToken.objects.get(key=mobile, code=code , createdAt__gt = threshold)
             except VerificationToken.DoesNotExist:
-                verification_result = verifiy(mobile, code)
-                verification_json = verification_result.json()
-
-                if verification_json.get('errorCode', 0) == 40003:
-                    raise MobileVerificationError()
-
-                if verification_result.status_code != 200:
-                    raise ServiceUnavailable()
-
+                raise MobileVerificationError()
 
         try:
             user = User.objects.get(mobile=mobile)
