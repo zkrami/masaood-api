8026153af1d3ec905955ee3a926c8618bacea53f
Rami Al-Zebak <17zrami@gmail.com>
2019-08-29 16:35:55 +0300

    order user filter

--- order/filters.py
+++ order/filters.py
@@ -1,24 +1,25 @@
-from order import models 
+from order import models
 import rest_framework_filters as filters
+from user.filters import UserFilter
+from user.models import User
 
 
 class OrderFilter(filters.FilterSet):
+    user = filters.RelatedFilter(UserFilter, field_name='user', queryset=User.objects.all())
 
     class Meta:
         model = models.Order
         fields = {
-            'user' : "__all__" , 
-            'center' : "__all__" , 
-            'deliveryAddress' : "__all__" , 
-            'deliveryLat' : "__all__" , 
-            'devliveryLng' : "__all__" , 
-            'total' : "__all__" , 
-            'status' : "__all__" , 
-            'createdAt' : "__all__" , 
-            'canceledAt' : "__all__" , 
-            'deliveredAt' : "__all__" , 
-            'assignedAt' : "__all__" , 
-            'deleted' : "__all__" , 
-            'archived' : "__all__" 
+            'center': "__all__",
+            'deliveryAddress': "__all__",
+            'deliveryLat': "__all__",
+            'devliveryLng': "__all__",
+            'total': "__all__",
+            'status': "__all__",
+            'createdAt': "__all__",
+            'canceledAt': "__all__",
+            'deliveredAt': "__all__",
+            'assignedAt': "__all__",
+            'deleted': "__all__",
+            'archived': "__all__"
         }
-
